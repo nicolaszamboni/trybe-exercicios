92d775c84c4f8cb28f28d03eea83c1eee0c4eb5d
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-20 16:11:23 -0300

    para fixar 4-2

---
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/parafixar4-2.js
@@ -0,0 +1,9 @@
+let weekDay = "quarta-feira"
+
+if (weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "segunda-feira" || weekDay === "sexta-feira" || weekDay === "terça-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay === "sabado" || weekDay === "domingo"){
+    console.log("FINALMENTE, descanso merecido UwU");
+} else{
+    console.log("Onde estou")
+}
