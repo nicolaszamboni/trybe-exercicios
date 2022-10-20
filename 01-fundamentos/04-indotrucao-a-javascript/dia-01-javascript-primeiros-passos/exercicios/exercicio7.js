7126062d7e5fba0d723990064d3410a761e2a3e0
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-20 18:14:44 -0300

    exercicio 9 - finalizado

---
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/exercicio7.js
@@ -0,0 +1,14 @@
+// Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+
+const num1 = 8;
+const num2 = 8;
+const num3 = 8;
+
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
