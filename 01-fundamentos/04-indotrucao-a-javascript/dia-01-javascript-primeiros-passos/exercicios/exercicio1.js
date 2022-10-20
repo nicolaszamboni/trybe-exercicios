4a34ee0479e88ebd38148c85fba81e5f1ca77c12
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-20 17:09:00 -0300

    exercicio 4 - finalizado

--- 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
@@ -1,3 +1,5 @@
+// Utilize if/else para fazer um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
 const num1 = 40;
 const num2 = 60;
 const num3 = 50;
