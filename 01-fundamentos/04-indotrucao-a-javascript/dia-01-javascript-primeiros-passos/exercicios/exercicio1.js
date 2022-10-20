337c2e51a08083a338475645635d05ff1e811597
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-20 17:01:10 -0300

    exercicio 3 - finalizado

--- 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
@@ -1,15 +1,13 @@
-// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
-// Adição (a + b)
-// Subtração (a - b)
-// Multiplicação (a * b)
-// Divisão (a / b)
-// Módulo (a % b)
+const num1 = 40;
+const num2 = 60;
+const num3 = 50;
 
-let num1 = 20;
-let num2 = 10;
-
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 % num2);
+if(num1 > num2 && num1 > num3){
+    console.log(num1);
+}
+else if(num2 > num1 && num2 > num3){
+    console.log(num2);
+}
+else{
+    console.log(num3)
+}
