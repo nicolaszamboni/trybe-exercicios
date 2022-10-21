969875c1c116cf9c96227f89a92397e0de5f9ce9
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-21 16:39:15 -0300

    terceira parte finalizada

--- 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
@@ -19,11 +19,19 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
+
+//     console.log(numbers[i]);
+
+//     soma += numbers[i];
+// }
+// console.log(soma);
+
+
 
 for (let i = 0; i < numbers.length; i++){
-    // console.log(numbers[i]);
     soma += numbers[i];
+    media = soma / numbers.length;
 }
-console.log(soma);
 
-
+console.log(media);
