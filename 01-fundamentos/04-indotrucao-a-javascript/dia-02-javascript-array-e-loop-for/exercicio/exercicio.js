19d27c60b4430620e0610a437d3c9fb679b0fe2f
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-21 18:10:04 -0300

    oitavo exercicio finalizado

--- 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
@@ -62,6 +62,14 @@
 // }
 // console.log(impares);
 
+// SETIMO EXERCICIO
+// for (let i = 0; i < numbers.length; i++){
+// if (numbers[i] < numeroMenor){
+// numeroMenor = numbers[i];
+// }
+// }
+// console.log(numeroMenor);
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
@@ -69,9 +77,6 @@ let numeroMaior = 0;
 let impares = 0;
 let numeroMenor = 105;
 
-for (let i = 0; i < numbers.length; i++){
-    if (numbers[i] < numeroMenor){
-        numeroMenor = numbers[i];
-    }
-    }
-    console.log(numeroMenor);
+for (let i = 1; i <= 25; i++){
+    console.log(i)
+}
