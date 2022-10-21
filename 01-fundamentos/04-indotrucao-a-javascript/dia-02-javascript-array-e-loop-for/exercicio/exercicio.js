79a2ffc50271a5abc80b3a91ca3eee1c1d72c0a5
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-21 17:41:02 -0300

    quinto exercicio finalizado

--- 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio/exercicio.js
@@ -32,19 +32,24 @@
 // }
 // console.log(media);
 
+// QUARTO EXERCICIO
+// soma += numbers[i];
+// media = soma / numbers.length;
+// }
+// if (media > 20){
+//     console.log('valor maior que 20')
+// }else {
+//     console.log('valor menor ou igual a 20')
+// }
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
+let numeroMaior = 0;
 
 for (let i = 0; i < numbers.length; i++){
-    soma += numbers[i];
-    media = soma / numbers.length;
-}
-    if (media > 20){
-        console.log('valor maior que 20')
-    }else {
-        console.log('valor menor ou igual a 20')
+    if (numbers[i] > numeroMaior){
+        numeroMaior = numbers[i];
     }
-
-    
-    
+}
+console.log(numeroMaior);
