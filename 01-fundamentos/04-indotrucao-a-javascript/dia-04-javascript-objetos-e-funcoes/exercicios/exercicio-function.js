940583eccc32892628d7b3998e85728fd274b63b
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-25 17:56:03 -0300

    exercicio 5 functions finalizado

--- 01-fundamentos/04-indotrucao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/exercicio-function.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/exercicio-function.js
@@ -68,9 +68,29 @@ console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joan
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 // Valor esperado no retorno da função: 2.
 
+function numerosRepetidos(inteiros){
+    let maisRepetidos = 0;
+    for (let index in inteiros){
+        if (inteiros[index] === inteiros){
+            maisRepetidos += 1;
+        }
+    }
+    return maisRepetidos;
+}
+console.log(numerosRepetidos([2, 3, 2, 5, 8, 2, 3]));
+
 // Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
 // Valor de teste: N = 5.
 // Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+
+function soma(numero){
+    let somaTotal = 0;
+    for (let index = 1; index <= numero; index++){
+        somaTotal += index;
+    }
+    return somaTotal;
+}
+console.log(soma(5));
 
 // Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
 // Valor de teste: 'trybe' e 'be'
