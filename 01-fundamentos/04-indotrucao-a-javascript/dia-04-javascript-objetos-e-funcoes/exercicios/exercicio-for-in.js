b74f6109662c2264d726e453559968e24230d92c
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-25 17:12:31 -0300

    exercicios bonus for in aula 4-4

--- 01-fundamentos/04-indotrucao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/exercicio-for-in.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/exercicio-for-in.js
@@ -15,21 +15,30 @@ let info = {
 };
 let info2 = {
     personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, , Dell’s Four Color Comics #178',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
     nota: 'O último MacPatinhas',
     recorrente: 'Sim',
 }
 
-// console.log('Bem-vinda, '+ info.personagem);
-// console.log(info.recorrente);
+console.log('Bem-vinda, '+ info.personagem);
 
-// for (let key in info){
-//     console.log(key);
-// }
+// Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
 
-// for (let key in info){
-//     console.log(info[key]);
-// }
+console.log(info.recorrente);
+
+// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for (let key in info){
+    console.log(key);
+}
+
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+for (let key in info){
+    console.log(info[key]);
+}
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
 
 for (let key in info){
     if (key === 'recorrente' && info2[key] === 'Sim' && info[key] === 'Sim'){
