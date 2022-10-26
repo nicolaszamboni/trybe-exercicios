a7e5b643258c7e42235938e6f4aea62621248aad
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-26 18:24:35 -0300

    exercicio 2-2 aula 4-5

---
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-05-javascript-ES6/exercicio2-2.js
@@ -0,0 +1,22 @@
+// 🚀 Crie uma função que calcule a área de um círculo.
+// Na matemática, a fórmula para calcular a área de um círculo é: área = PI vezes o raio elevado ao quadrado.
+// Com base nessa informação:
+// Crie a função circleArea, que recebe o valor do raio como parâmetro, utilizando arrow functions;
+// Crie uma variável para armazenar o valor de PI (lembre-se de armazenar o tipo de variável da forma correta);
+// Crie a lógica para retornar a área do círculo;
+// Imprima no terminal “Essa é a área do círculo: resultado da função“ (lembre-se de utilizar o template literals nesse momento).
+
+// Exemplo de retorno:
+// Parâmetro: 3
+// Retorno: Essa é a área do círculo: 28.259999999999998
+// Parâmetro: 5
+// Retorno: Essa é a área do círculo: 78.5
+
+const circleArea = (raio) => {
+
+let pi = 3.14;
+let areaCirculo = pi * (raio * 2);
+
+return console.log(`Essa é a área do círculo: ${areaCirculo}`)
+}
+circleArea(2);
