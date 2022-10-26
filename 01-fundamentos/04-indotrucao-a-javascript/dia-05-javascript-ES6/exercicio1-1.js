503e9e15a6a5d1a9f7ec3e780da40cf631d4c093
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-26 17:28:56 -0300

    exercicio 1.4 aula 4-5

--- 01-fundamentos/04-indotrucao-a-javascript/dia-05-javascript-ES6/exercicio1-1.js
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-05-javascript-ES6/exercicio1-1.js
@@ -1,13 +1,26 @@
 // 🚀 Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
 
-    // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
-    let pessoa = {
-        nome: 'Henri',
-        idade: 20
-      }
-      pessoa = {
-        nome: 'Luna',
-        idade: 19
-      } // Altere essa estrutura para corrigir o erro.
-      console.log('Nome:', pessoa.nome);
-      console.log('Idade:', pessoa.idade);
+    // // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+    // let pessoa = {
+    //     nome: 'Henri',
+    //     idade: 20
+    //   }
+    //   pessoa = {
+    //     nome: 'Luna',
+    //     idade: 19
+    //   } // Altere essa estrutura para corrigir o erro.
+    //   console.log('Nome:', pessoa.nome);
+    //   console.log('Idade:', pessoa.idade);
+    
+        // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+        const pessoa = {
+            nome: 'Henri',
+            idade: 20
+        }
+
+            pessoa.nome = 'Luna';
+            pessoa.idade = 19;
+
+        // Altere essa estrutura para corrigir o erro.
+          console.log('Nome:', pessoa.nome);
+          console.log('Idade:', pessoa.idade);
