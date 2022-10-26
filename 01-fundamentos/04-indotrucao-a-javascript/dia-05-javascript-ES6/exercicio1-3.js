fb70064fec178bca076bf3071ecc3c25551bf606
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-10-26 17:21:23 -0300

    exercicio 1.3 aula 4-5

---
+++ 01-fundamentos/04-indotrucao-a-javascript/dia-05-javascript-ES6/exercicio1-3.js
@@ -0,0 +1,13 @@
+// 🚀 Transforme a função hello em uma arrow function;
+// Copie o código abaixo:
+
+//     function hello(nome) {
+//       return `Olá, ${nome}!`
+//     }
+//     let nome = 'Ivan';
+//     console.log(hello(nome));
+
+const hello = nome => `Olá, ${nome}!`
+
+let nome = 'Ivan';
+console.log(hello(nome));
