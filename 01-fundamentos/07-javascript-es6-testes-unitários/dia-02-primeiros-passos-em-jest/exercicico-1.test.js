f3cb9acebe70703d4323b9d00a7961cff4ac1cdc
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-11-22 20:15:40 -0300

    exercicio 1 aula 7-2 finalizado

--- 01-fundamentos/07-javascript-es6-testes-unitários/dia-02-primeiros-passos-em-jest/exercicico-1.test.js
+++ 01-fundamentos/07-javascript-es6-testes-unitários/dia-02-primeiros-passos-em-jest/exercicico-1.test.js
@@ -1,7 +1,9 @@
-const myRemove = require('../dia-02-primeiros-passos-em-jest/exercicico-1');
+const myRemove = require('./exercicico-1')
 
 describe('a função myRemove', () => {
-    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;', () => {
+    it('Verifique se a chamada myRemove retorna o array esperado;', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
     });
 });
