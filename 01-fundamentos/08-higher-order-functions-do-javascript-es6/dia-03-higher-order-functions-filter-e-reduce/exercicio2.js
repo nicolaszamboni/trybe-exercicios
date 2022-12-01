e8209fbd3792f95781852cb74dcaa4a7f0d168c0
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2022-12-01 20:01:49 -0300

    exercicio 2 aula 8-3 parte 4 e 5 finalizado

--- 01-fundamentos/08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicio2.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicio2.js
@@ -121,4 +121,26 @@ const countA = () => {
     }, 0)
 };
 
-console.log(countA())
+// console.log(countA());
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const expectedResult5 = [
+ { name: 'Pedro Henrique', average: 7.8 },
+ { name: 'Miguel', average: 9.2 },
+ { name: 'Maria Clara', average: 8.8 },
+];
+
+const studentAverage = () => {
+  return nameAndGrade = students.map((student, index) => (
+    {
+        name: student,
+        average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+    }
+  ))
+}
+
+console.log(studentAverage())
