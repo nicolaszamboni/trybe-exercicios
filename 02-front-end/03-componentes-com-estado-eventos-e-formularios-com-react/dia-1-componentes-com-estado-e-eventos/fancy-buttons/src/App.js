31186fc872f619e42957a57712385ff759c71e2a
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-01-25 15:34:04 -0300

    exercicio de fixação 3 finalizado

--- 02-front-end/03-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-front-end/03-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -6,25 +6,40 @@ class App extends React.Component {
     super();
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }
   }
   handleClick1() {
     console.log(this);
     console.log("Clicou no botão 1!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1,
+    }));
   }
   handleClick2() {
     console.log(this);
     console.log("Clicou no botão 2!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1,
+    }));
   }
   handleClick3 = () => {
     console.log(this);
     console.log("Clicou no botão 3!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1,
+    }));
   }
   render() {
+    const { numeroDeCliques1, numeroDeCliques2, numeroDeCliques3 } = this.state;
     return (
       <>
-        <button onClick={this.handleClick1}>Meu botão 1</button>
-        <button onClick={this.handleClick2}>Meu botão 2</button>
-        <button onClick={this.handleClick3}>Meu botão 3</button>
+        <button onClick={this.handleClick1}>{ numeroDeCliques1 }</button>
+        <button onClick={this.handleClick2}>{ numeroDeCliques2 }</button>
+        <button onClick={this.handleClick3}>{ numeroDeCliques3 }</button>
       </>
     );
   }
